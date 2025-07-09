74461b0f3bfbaff3b210cfc9117950901de414a4
Vinicius Mendes <mendesvv96@gmail.com>
2025-07-09 18:16:28 -0300

    corrigindo

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,10 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://gycriyugpnvlvbdltvwf.supabase.co"; // Pegue no Supabase
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd5Y3JpeXVncG52bHZiZGx0dndmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI2NDUzNjIsImV4cCI6MjA1ODIyMTM2Mn0.t93mIb4je86ohTE20FVepBcercuiZGxgk83Qe0iNJog"; // Pegue no Supabase
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+
 
 // Testando conexão
 (async () => {
